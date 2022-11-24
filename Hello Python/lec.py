8ba5b99e8a0dbe60d8d2936942c1db259341a709
Post-Nataly <post-design@mail.ru>
2022-11-24 14:01:47 +0800

    задача1

---
+++ Hello Python/lec.py
@@ -0,0 +1,67 @@
+print('hello world')
+
+# a = 123
+# b = 1.23
+# print(a + b)
+
+# print(type(a))
+
+s = '"hello" world!'
+# print(s)
+
+# print(a, ' - ', b, s)
+# print('{1} - {2} - {0}'.format(a, b, s))
+
+# print(f'{a} - {b} - {s}')
+
+# f = False
+# print(f)
+
+# list = [1, 2, 'строка']
+# print(list)
+
+# print('введите а');
+# a = input()
+# print('введите b');
+# b = input()
+# print(a,' + ', b, ' = ', a + b)
+# print('{} {}'.format(a, b))
+# print(f'{a} {b}')
+
+# print('введите а');
+# a = int(input())
+# print('введите b');
+# b = float(input())
+# print(a,' + ', b, ' = ', a + b)
+
+# a = 1.258896
+# b = 5
+# c = round(a * b, 3)
+# print(c)
+
+# a = 1 < 4 and 5 > 2
+# print(a)
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+# original = 23
+# inverted = 0
+# while original != 0:
+#     inverted = inverted * 10 + (original % 10)
+#     original //= 10
+# print(inverted)
+
+# list = [1, 2, 3, 8, 5]
+# for i in list:
+#     print(i)
+
+# for i in range(1, 20, 3):
+#     print(i)
+
+# for i in 'какая-то строка':
+#     print(i)
