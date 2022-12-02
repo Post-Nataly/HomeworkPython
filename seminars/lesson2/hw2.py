6965a700a9ce0e73a1af913285749ab8553e0e17
Post-Nataly <post-design@mail.ru>
2022-12-02 13:48:57 +0800

    hw2-1правильно

--- seminars/lesson2/hw2.py
+++ seminars/lesson2/hw2.py
@@ -1,10 +1,15 @@
 # 1. Программа принимает на вход вещественное число и показывает сумму его цифр.
 
-a = float(input("Введите дробь: "))
+num = input("Введите дробь: ")
+mass = num.split(".")
+a = int(mass[0])
+b = int(mass[1])
 sum = 0
-while a%10 != 0:
-    b = (a * 10) % 10
-    sum += b
-    a = a * 10
+while (a != 0):
+    sum += a%10
+    a = a // 10
+while (b != 0):
+    sum += b%10
+    b = b //10
 
 print(int(sum))
