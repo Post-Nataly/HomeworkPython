719d11e1187c7e9fd3b10cebf2768fc16a0dd5b3
Post-Nataly <post-design@mail.ru>
2022-12-07 11:55:05 +0800

    message1

--- seminars/lesson2/hw2.py
+++ seminars/lesson2/hw2.py
@@ -1,16 +1,16 @@
 # 1. Программа принимает на вход вещественное число и показывает сумму его цифр.
 
 num = input("Введите дробь: ")
-mass = num.split(".")
+mass = num.split('.')
 a = int(mass[0])
 b = int(mass[1])
 sum = 0
 while (a != 0):
-    sum += a%10
+    sum += a %10
     a = a // 10
 while (b != 0):
     sum += b%10
-    b = b //10
+    b = b // 10
 print(int(sum))
 
 
