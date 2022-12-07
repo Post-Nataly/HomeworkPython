d348a6c58a22560177347be4e9dcd1e93961efe5
Post-Nataly <post-design@mail.ru>
2022-12-07 11:41:04 +0800

    hw2-3

--- seminars/lesson2/hw2.py
+++ seminars/lesson2/hw2.py
@@ -1,18 +1,19 @@
 # 1. Программа принимает на вход вещественное число и показывает сумму его цифр.
 
-# num = input("Введите дробь: ")
-# mass = num.split(".")
-# a = int(mass[0])
-# b = int(mass[1])
-# sum = 0
-# while (a != 0):
-#     sum += a%10
-#     a = a // 10
-# while (b != 0):
-#     sum += b%10
-#     b = b //10
+num = input("Введите дробь: ")
+mass = num.split(".")
+a = int(mass[0])
+b = int(mass[1])
+sum = 0
+while (a != 0):
+    sum += a%10
+    a = a // 10
+while (b != 0):
+    sum += b%10
+    b = b //10
+print(int(sum))
 
-# print(int(sum))
+
 
 # 2. Программа принимает на вход число N и выдает набор произведений чисел от 1 до N.
 
@@ -27,15 +28,17 @@
 #     numbers.append(a)
 # print(numbers)
 
+
+
 # 3. Задайте список из n чисел последовательности (1 + 1/n)^n и выведите на экран их сумму.
 
 # n = int(input("n = "))
-# lst = [round((1+1/i)**i, 3) for i in range(1, n+1)]
+# lst = [round((1+1/i)**i, 3)
+# for i in range(1, n+1)]
 # print(f'Последовательность: {lst}\nСумма: {round(sum(lst), 3)}')
 
-n = int(input("n = "))
-lst = {}
-for i in range(1, n+1):
-    round((1 + 1/i)**i, 2)
-    lst.append(i)
-print(f"Для n = {n} {lst} Cумма {round(sum(lst), 2)}")
+
+# 4. Задайте список из N элементов, заполненных числами из промежутка [-N, N].
+# Найдите произведение элементов на указанных индексах. Индексы вводятся одной строкой, через пробел.
+
+# n = int(i
