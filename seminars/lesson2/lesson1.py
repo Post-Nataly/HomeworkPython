ac14c74be5c40b9d21003e05525da55524cf62d5
Post-Nataly <post-design@mail.ru>
2022-12-10 11:53:34 +0800

    работа с файлами, импорт функций

---
+++ seminars/lesson2/lesson1.py
@@ -0,0 +1,13 @@
+# функции
+
+def f(x):
+    if x == 1:
+        return 'целое'
+    elif x == 2.3:
+        return 23
+    else:
+        return
+
+# arg = 1
+# print(f(arg))          # целое
+# print(type(f(arg)))    # <class 'str'>
--- seminars/lesson2/lesson2.py
+++ seminars/lesson2/lesson2.py
@@ -1,5 +1,29 @@
 # работа с файлами
-colors = ['red', 'green', 'blue']
-data = open('file.txt', 'a') # а - добавление данных, r - чтение, w - запись
-data.writelines(colors) # для записи набора данных (без разделителей)
-data.close() # закрыть файл
+
+# colors = ['red', 'green', 'blue']
+# data = open('file.txt', 'a') # а - добавление данных, r - чтение, w - запись
+# data.writelines(colors) # для записи набора данных (без разделителей)
+# data.close() # закрыть файл
+# colors = ['red', 'green', 'white']
+# data = open('file.txt', 'w')
+# data.write('\nLINE 2\n')
+# data.write('LINE 3\n')
+# data.close()
+
+# другой вариант открытия файла:
+# with open('file.txt', 'a') as data:
+#     data.write('\nline 111\n')
+#     data.write('line 222\n')
+
+# path = 'file.txt'        # создали путь к папке
+# data = open(path, 'r')   # открыли для чтения
+# for line in data:        # проходим по файлу, считывая все строки
+#     print(line)          # выводим
+# data.close               # закрываем
+
+# exit() # чтобы не читался код ниже
+
+# импорт функции из файла
+
+import lesson1
+print(lesson1.f(1))
