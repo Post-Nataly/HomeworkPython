e27d325a8e1ed1f6e0682efa430c5eb8889e7cc7
Post-Nataly <post-design@mail.ru>
2022-12-13 20:12:48 +0800

    hw-3

--- seminars/lesson2/hw3.py
+++ seminars/lesson2/hw3.py
@@ -1,89 +1,82 @@
 # 1. Программа находит сумму элементов списка, стоящих на нечётных идексах.
 
-# from random import randrange
-# a = [randrange(1, 10) for i in range(6)]
-# print("список: ")
-# print(a)
-# s = 0
-# for i in range(len(a)):
-#     if (i % 2 != 0):
-#         s += a[i]
-# print("сумма элементов на нечетных индексах:")
-# print(s)
+from random import randrange
+
+a = [randrange(1, 10) for i in range(6)]
+print("список: ")
+print(a)
+
+s = 0
+for i in range(len(a)):
+    if (i % 2 != 0):
+        s += a[i]
+
+print("сумма элементов на нечетных индексах:")
+print(s)
 
 
 
 # 2. Программа найдёт произведение пар чисел списка. 
 #    Пара - первый и последний элемент, второй и предпоследний и т.д.
 
-# from random import randrange
-# a = [randrange(1, 10) for i in range(11)]
-# print("список: ")
-# print(a)
-# b = []
-# m = 0
-# j = 0
-# for i in range(len(a)):
-#     if j + i < len(a):
-#         m = a[i] * a[len(a)-1-j]
-#         b.append(m)
-#         j += 1
-# print("произведения пар:")
-# print(b)
+from random import randrange
+
+a = [randrange(1, 10) for i in range(11)]
+print("список: ")
+print(a)
+
+b = []
+m = 0
+j = 0
+for i in range(len(a)):
+    if j + i < len(a):
+        m = a[i] * a[len(a)-1-j]
+        b.append(m)
+        j += 1
+
+print("произведения пар:")
+print(b)
 
 
 # 3. Программа ищет разницу между максимальным и минимальным значением дробной части элементов списка из вещественных чисел.
 
 # - [1.1, 1.2, 3.1, 10.01] => 0.19
 
-# import random
-# a = [float(random.random()*10) for i in range(7)]
-# b = []
-# for i in a:
-#     b.append('%.2f' % i)
-# b = [float(x) for x in b]
-# print("список: ")
-# print(b)
-# c = []
-# for i in b:
-#     i *= 100
-#     i = i % 100
-#     c.append(int(i))
-# print('%.2f' % (max(c)/100 - min(c)/100))
+import random
+
+a = [float(random.random()*10) for i in range(7)]
+b = []
+for i in a:
+    b.append('%.2f' % i)
+b = [float(x) for x in b]
+print("список: ")
+print(b)
+
+c = []
+for i in b:
+    i *= 100
+    i = i % 100
+    c.append(int(i))
+print('%.2f' % (max(c)/100 - min(c)/100))
 
 
 
-# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+# 4. Программа преобразовывает десятичное число в двоичное.
 
-# Пример:
+a = int(input('Введите число: '))
 
-# - 45 -> 101101
-# - 3 -> 11
-# - 2 -> 10
-
-# a = 1
-# def binary(n):
-#     while n > 0:
-#         if (n % 2 == 0):
-#             return 0
-#         else:
-#             return 1
-#     n = round(n / 2)
-# c = []
-# for i in c:
-#     c.append(binary(a))
-# print(*c)
-# print(binary(a))
+b = ''
+while a > 0:
+    b = str(a % 2) + b
+    a = a // 2
+  
+print(b)
 
 
 
-# Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
-
-# Пример:
-
-# - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+# 5. Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
         
-k = 8
+k = int(input('Введите число: '))
 
 def fib(n):
     if n in [1, 2]:
@@ -94,6 +87,7 @@ def fib(n):
 list1 = []
 for e in range(1, k+1):
     list1.append(fib(e))
+
 list2 = list1[::-1]
 for e in range(1, k+1):
     if (len(list2) / 2 != 0):
