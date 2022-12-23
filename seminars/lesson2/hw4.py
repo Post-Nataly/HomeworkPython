2f8e804c0925d34ad1b60f0bca506ddb14588d7d
Post-Nataly <post-design@mail.ru>
2022-12-23 20:55:41 +0800

    hw4-5

--- seminars/lesson2/hw4.py
+++ seminars/lesson2/hw4.py
@@ -9,37 +9,37 @@
 #     Ввод: 0.001
 #     Вывод: 3.141
 
-# import math
+import math
 
-# π = math.pi
-# d = 0.001
+π = math.pi
+d = 0.001
 
-# d = str(d)
-# i = '.'
-# if i in d:
-#     a = len(d) - d.index(i) - 1
-#     π = round(π, a)
-#     print(π)
-# else:
-#     print('число d целое')
+d = str(d)
+i = '.'
+if i in d:
+    a = len(d) - d.index(i) - 1
+    π = round(π, a)
+    print(π)
+else:
+    print('число d целое')
 
 
 
 # 2. Программа составляет список простых множителей числа N.
 
-# n = int(input('Введите число n: '))
-# a = []
-# x = 2
-# while x * x <= n:
-#     if n % x == 0:
-#         a.append(x)
-#         n //= x
-#     else:
-#         x += 1
-# else:
-#     a.append(n)
+n = int(input('Введите число n: '))
+a = []
+x = 2
+while x * x <= n:
+    if n % x == 0:
+        a.append(x)
+        n //= x
+    else:
+        x += 1
+else:
+    a.append(n)
 
-# print(a)
+print(a)
 
 
 
@@ -47,24 +47,24 @@
 # Ввод: [1, 1, 2, 3, 4, 4, 4]
 # Вывод: [2, 3]
 
-# lst = [1, 1, 2, 3, 4, 4, 4]
+lst = [1, 1, 2, 3, 4, 4, 4]
 
-# numb_repit = []
-# for i in lst:
-#     count = 0
-#     for j in lst:
-#         if i == j:
-#             count += 1
-#     numb_repit.append(count)
+numb_repit = []
+for i in lst:
+    count = 0
+    for j in lst:
+        if i == j:
+            count += 1
+    numb_repit.append(count)
 
-# uniq = set()
-# index = 0
-# while index < len(lst):
-#     if numb_repit[index] == 1:
-#         uniq.add(lst[index])
-#     index += 1
+uniq = set()
+index = 0
+while index < len(lst):
+    if numb_repit[index] == 1:
+        uniq.add(lst[index])
+    index += 1
         
-# print(list(uniq))
+print(list(uniq))
 
 
 # 4. Задана натуральная степень k. 
@@ -76,24 +76,25 @@
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 # - k=4 => 8*(x**4) + 9*(x**3) + 1*(x**2) + 5*x + 4 = 0 или 8*(x**4) + 5*x + 4 = 0 и т.д.
 
-# data = open('seminars/lesson2/file.txt', 'a')
+data = open('seminars/lesson2/file.txt', 'a')
 
-# from random import randint
+from random import randint
 
-# k = int(input(''))
+k = int(input(''))
 
-# coefficients = []
-# for i in range(k+1):
-#     coefficients.append(randint(0, 100))
-# while k > 2:
-#     for i in range(k-1):
-#         data.write(str(f"{coefficients[i]} * (x ** {k}) + "))
-#         k -= 1
-#         i += 1
-# else:
-#     data.write(str(f"{coefficients[-2]} * x + {coefficients[-1]} = 0"))
+coefficients = []
+for i in range(k+1):
+    coefficients.append(randint(0, 100))
+while k > 2:
+    for i in range(k-1):
+        data.write(str(f"{coefficients[i]} * (x ** {k}) + "))
+        k -= 1
+        i += 1
+else:
+    data.write(str(f"{coefficients[-2]} * x + {coefficients[-1]} = 0"))
 
-# data.close()
+data.close()
+
 
 
 # 5. Даны два файла, в каждом из которых находится запись многочлена.
@@ -103,69 +104,73 @@
 data = open('seminars/lesson2/file.txt', 'r')
 
 poly1 = data.readline()
+print(poly1)
+
+poly1 = poly1.split(' ')
+
+polynom1 = list(reversed(poly1))
+
+polynom1[2] = int(polynom1[2])
+
+a = 0
+while a < len(polynom1) + 1:
+    polynom1[a] = int(polynom1[a])
+    a += 6
 
 data = open('seminars/lesson2/newfile.txt', 'r')
 
 poly2 = data.readline()
-
-poly1 = poly1.split(' ')
-def to_int(number):
-    try:
-        return int(number)
-    except ValueError:
-        return int("nan")
-
-print(list(map(to_int, poly1)))
-
-key1 = []
-a = 0
-for i in poly1:
-    key1.append(a)
-    a += 1
-keyy1 = reversed(key1)
-
-poly1_dict = dict(zip(keyy1, poly1))
-print(poly1_dict)
+print(poly2)
 
 poly2 = poly2.split(' ')
 
-key2 = []
+polynom2 = list(reversed(poly2))
+
+polynom2[2] = int(polynom2[2])
+
 a = 0
-for i in poly2:
-    key2.append(a)
-    a += 1
-keyy2 = reversed(key2)
+while a < len(polynom2) + 1:
+    polynom2[a] = int(polynom2[a])
+    a += 6
 
-poly2_dict = dict(zip(keyy2, poly2))
-print(poly2_dict)
+polynom3 = []
+if len(polynom1) > len(polynom2):
+    for i in polynom1:
+        polynom3.append(i)
+    a = 2
+    while a < len(polynom2):
+        if a == 2:
+            polynom3[a] = polynom1[a] + polynom2[a]
+            a += 4
+        if a == 6:
+            polynom3[a] = polynom1[a] + polynom2[a]
+            a += 6
+else:
+    for i in polynom2:
+        polynom3.append(i)
+    a = 2
+    while a < len(polynom1):
+        if a == 2:
+            polynom3[a] = polynom1[a] + polynom2[a]
+            a += 4
+        if a == 6:
+            polynom3[a] = polynom1[a] + polynom2[a]
+            a += 6
+        polynom3[a] = polynom1[a] + polynom2[a]
+        a += 6
 
+polynom3[2] = str(polynom3[2])
 
-# for i in poly1_dict.items() & poly2_dict.items():
-#     print(i, poly1_dict[i], poly2_dict[i])
+a = 0
+while a < len(polynom3) + 1:
+    polynom3[a] = str(polynom3[a])
+    a += 6
 
-# dictionary = {}
-# dic = 0
-# # if len(poly1_dict) >= len (poly2_dict):
-# for i in poly2_dict.keys():
-#     for j in poly1_dict.keys():
-#         if i == j:
-#             if poly2_dict[i] == int():
-#                 dic = poly2_dict[i] + poly1_dict[j]
-#                 dictionary = dict.fromkeys(dic)
-#             else:
-#                 continue
-#         else:
-#             continue
-# else:
-    # for j in poly1_dict.keys():
-    #     for i in poly2_dict.keys():
-    #         if i == j:
-    #             if poly1_dict[j] == int():
-    #                 poly1_dict[j] + poly2_dict[i]
-    #                 dictionary = dict.fromkeys(poly1_dict[j], poly2_dict[i])
-    #             else:
-    #                 continue
-    #         else:
-    #             continue    
-        
-# print(dictionary)
+pol_str = list(reversed(polynom3))
+
+pol_end = " ".join(pol_str)
+print(pol_end)
+
+data = open('seminars/lesson2/lostfile.txt', 'a')
+data.write(pol_end)
+data.close() 
